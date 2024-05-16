eba179127dea35c23e7f202b481ca4eda6538c51
n8fr8 <nathan@guardianproject.info>
2024-05-16 14:09:49 -0400

    remove for the proofmode fork - use the library instead

--- android-libproofmode/src/main/java/org/witness/proofmode/ProofModeConstants.java
+++
@@ -1,9 +0,0 @@
-package org.witness.proofmode;
-
-public interface ProofModeConstants {
-
-        public static final String PREFS_KEY_PASSPHRASE = "pgpkp";
-        public static final String PREFS_KEY_PASSPHRASE_DEFAULT = "password";
-
-
-}
